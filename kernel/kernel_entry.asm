1ca31f395866f1250594a1ef2b106ec0cd5d6340
Shubham Sharma <shubhamsharma1172@gmail.com>
2019-03-09 09:16:02 +0530

    added _start label

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,6 +1,8 @@
 ; Jump into kernel's entry function
 [bits 32]
 [extern main]           ; declares usage of external symbol main
+global _start           ; declaration for ld
 
+_start:
 call main               ; invoke main() in kernel
 jmp $                   ; hang forever on returning
